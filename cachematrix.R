ae0fadb56214e1a97199950f81758786867887ca
Samb32 <samuel.baird@cardinalhealth.com>
2025-03-28 11:11:29 -0700

    Update cacheMatrix.R
    Created caching functions per programming assignment 2 instructions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Step 1: Create a square matrix object to test the function by using the 
+# matrix function
+my2X2_matrix <- matrix(1:4, 2, 2)
+my2X2_matrix # print object to confirm squared matrix
+solve(my2X2_matrix) # verify that your matrix has an inverse
 
-## Write a short comment describing this function
+# Step 2: Create the makeCacheMatrix function based on the example provided for 
+#caching the mean that both caches the matirx object and caches it's inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve  
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+# Step 3: Create a cache matrix object
+cached_mat <- makeCacheMatrix(my2X2_matrix)  
 
-## Write a short comment describing this function
-
+# Step 4: Create the cacheSolve function using the example to solve for the 
+# cached mean to either look for a cached object or solve the inverse of a matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+# Test of the cached object
+cacheSolve(cached_mat)
